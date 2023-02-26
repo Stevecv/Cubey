180f5cb410d344eb35e9eb0454ce9801660f09d1
Stevecv <mail.stevecv@gmail.com>
2023-02-26 16:29:50 +0000

    Installation

--- src/main/java/cubey/cubey/minecraft/Cubey.java
+++ src/main/java/cubey/cubey/minecraft/Cubey.java
@@ -1,13 +1,19 @@
 package cubey.cubey.minecraft;
 
+import cubey.cubey.general.install.Install;
 import cubey.cubey.minecraft.commands.utility.Command;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
 
 
 public final class Cubey extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        try {
+            new Install().install();
+        } catch (IOException e) {throw new RuntimeException(e);}
         addCommand(new cubey.cubey.minecraft.commands.Cubey());
     }
 
