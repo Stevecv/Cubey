3563445911cdb234bf129f65be818d7dc38aaa53
Jeroen <jeroen@vandegeest.eu>
2023-02-26 17:10:09 +0100

    add command

--- src/main/java/cubey/cubey/minecraft/Cubey.java
+++ src/main/java/cubey/cubey/minecraft/Cubey.java
@@ -1,5 +1,6 @@
 package cubey.cubey.minecraft;
 
+import cubey.cubey.minecraft.commands.utility.Command;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -7,10 +8,16 @@ public final class Cubey extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        addCommand(new cubey.cubey.minecraft.commands.Cubey());
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+
+    public void addCommand(Command command) {
+        getServer().getPluginCommand(command.getData().getName()).setExecutor(command);
     }
 }
