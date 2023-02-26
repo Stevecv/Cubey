982186f0b71e65ac273b73dbd0ac5a0a8f70b17d
Stevecv <mail.stevecv@gmail.com>
2023-02-26 14:11:48 +0000

    Recipie File

--- src/main/java/cubey/cubey/general/Recipie.java
+++ src/main/java/cubey/cubey/general/Recipie.java
@@ -1,5 +1,19 @@
 package cubey.cubey.general;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Recipie {
+    String path = null;
+    public Recipie(String path) {
+        this.path = path;
+    }
+
+    Path p = Paths.get(path);
+
+    private String getCode() throws IOException {return Files.readString(p, StandardCharsets.UTF_8);}
 
 }
