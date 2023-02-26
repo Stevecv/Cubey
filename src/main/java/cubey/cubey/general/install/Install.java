933a00aa7981e34eefa53321594441e6fdf8ab25
Stevecv <mail.stevecv@gmail.com>
2023-02-26 19:09:50 +0000

    Create projects from command

--- src/main/java/cubey/cubey/general/install/Install.java
+++ src/main/java/cubey/cubey/general/install/Install.java
@@ -1,5 +1,9 @@
 package cubey.cubey.general.install;
 
+import cubey.cubey.minecraft.Cubey;
+import org.bukkit.Bukkit;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,9 +11,14 @@ import java.nio.file.Paths;
 
 public class Install {
     public void install() throws IOException {
-        Path scriptsFolder = Paths.get("Cubey/Scripts");
-        if (!Files.exists(scriptsFolder)) { Files.createDirectory(scriptsFolder); }
-        Path compiled = Paths.get("Cubey/Built Scripts");
-        if (!Files.exists(compiled)) { Files.createDirectory(compiled); }
+        Cubey plugin = Cubey.getInstance();
+        File dataFile = new File(plugin.getDataFolder() +  File.separator);
+        File projects = new File(dataFile + File.separator + "projects" +  File.separator);
+        Bukkit.getConsoleSender().sendMessage(plugin.getDataFolder() + File.separator);
+
+        if(!dataFile.exists())
+            try { dataFile.mkdir(); } catch (Exception e) { e.printStackTrace(); }
+        if(!projects.exists())
+            try { projects.mkdir(); } catch (Exception e) { e.printStackTrace(); }
     }
 }
